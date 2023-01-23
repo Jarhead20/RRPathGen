d939b855f6d3cecef53472ceb6cbf4f1faa5ab31
Jarhead <j.a.lindsay06@gmail.com>
2023-01-23 11:22:37 +1000

    changed Trajectory to TrajectorySequence in the export

--- src/main/java/jarhead/ButtonPanel.java
+++ src/main/java/jarhead/ButtonPanel.java
@@ -73,7 +73,7 @@ public class ButtonPanel extends JPanel {
                     }
 
                     StringBuilder sb = new StringBuilder();
-                    sb.append(String.format("Trajectory %s = drive.trajectorySequenceBuilder(new Pose2d(%.2f, %.2f, Math.toRadians(%.2f)))%n",getCurrentManager().name, x, -y, (node.robotHeading +90)));
+                    sb.append(String.format("TrajectorySequence %s = drive.trajectorySequenceBuilder(new Pose2d(%.2f, %.2f, Math.toRadians(%.2f)))%n",getCurrentManager().name, x, -y, (node.robotHeading +90)));
                     for (int i = 1; i < getCurrentManager().size(); i++) {
                         node = getCurrentManager().get(i);
                         x = main.toInches(node.x);
